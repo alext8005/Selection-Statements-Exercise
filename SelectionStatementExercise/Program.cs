78171281cb98135b8abd8d4dc0036f048dd98ca3
AlexTrejo <alext8005@yahoo.com>
2025-05-13 22:34:53 -0600

    excercise 1 done

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNumber = r.Next(1, 10);
+            
+            Console.WriteLine("Guess my favorite number :");
+            int userInput = int.Parse(Console.ReadLine());
+
+            
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("You guessed it");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else
+            {
+                Console.WriteLine("Too high");
+            }
+
+
         }
+        
     }
 }
